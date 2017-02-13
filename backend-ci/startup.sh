11a13ab1d6f11d095d23407cbbb606c7a284665b
David Von Lehman <david.vonlehman@gmail.com>
2017-02-13 10:17:37 -0800

    Redoing the backend-ci image

--- backend-ci/startup.sh
+++ backend-ci/startup.sh
@@ -3,10 +3,10 @@ set -e
 
 # chown redis:redis /var/lib/redis
 # Start redis
-/usr/bin/redis-server /etc/redis/redis.conf
+/usr/local/bin/redis-server /etc/redis/redis.conf
 
 # Start dynamodb-local
 # nohup bash -c "java -Djava.library.path=./DynamoDBLocal_lib -jar DynamoDBLocal.jar -sharedDb -inMemory -port 8000 2>&1" &
 (cd /var/dynamodb-local; nohup bash -c "java -Djava.library.path=./DynamoDBLocal_lib -jar DynamoDBLocal.jar -sharedDb -inMemory -port 8000 2>&1" &)
-sleep "10s"
-cat /home/node/nohup.out
+sleep "5s"
+cat /var/dynamodb-local/nohup.out
